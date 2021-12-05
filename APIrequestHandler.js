794bb61f786e440d20aca0c2bb8abe1b098cf1b7
Sinthiya Balan <sinthiyamct@gmail.com>
2021-12-05 21:34:41 +0100

    Update API handler

--- APIrequestHandler.js
+++ APIrequestHandler.js
@@ -1,27 +1,51 @@
 
-function getWeatherForecast(form)
-{
-  var zipcode = form.zipcode.value;
-  console.log("welcome");
-  console.log(zipcode);
+document.querySelector("#weatherForm").addEventListener("submit", function (event) {
+  // stop form submission
+  event.preventDefault();
   
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
-    //document.getElementById("demo").innerHTML = this.responseText;
-  const obj = JSON.parse(this.responseText);
-  
-document.getElementById("demo").innerHTML = obj.timezone + ", " + obj.weather[0].main+
-", " +
-obj.weather[0].icon+","+
-obj.name + 
-","+ 
-obj.id;
+  const zip = document.getElementById("weatherForm").zipcode.value;
+  if(isNaN(zip)) {
+    alert("Zip code is not a valid Number!");
+    return;
   }
-  
-  //json["address"].city  json.address.streetAddress
-  xhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?zip=81549,DE&appid=5ad761e2beb630fe822fee4fcd952cf4");
-  xhttp.send();
-}
-
-
-  
+  const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() 
+    {
+      if(xhttp.readyState === 4) {
+        if(xhttp.status === 200) {
+          console.log("response:", this.responseText);
+          const obj = JSON.parse(this.responseText);
+          
+          
+           const responseElement = document.getElementById("displayResponse");
+           responseElement.innerHTML="";
+           const cityElement = document.createElement("h1");
+           cityElement.innerHTML = obj.name;
+           responseElement.appendChild(cityElement);
+        
+           const imgElement = document.createElement("img");
+          
+           imgElement.src ="http://openweathermap.org/img/w/" + obj.weather[0].icon + ".png";
+           imgElement.width = 100;
+           responseElement.appendChild(imgElement);
+                    
+           const temp = Math.round(obj.main.temp -273.15);
+           const tempElement = document.createElement("h2") ;
+           tempElement.innerHTML = temp+"°C";
+           responseElement.appendChild(tempElement);
+          
+        }
+        else {
+          const responseElement = document.getElementById("displayResponse");
+          responseElement.innerHTML="";
+          const errorElement = document.createElement("h4");
+          errorElement.innerHTML = "Error. City not found!";
+          responseElement.appendChild(errorElement);
+        }
+      }
+        
+    }
+    xhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?zip="+zip+",DE&appid=5ad761e2beb630fe822fee4fcd952cf4");
+    xhttp.send();
+    
+});
